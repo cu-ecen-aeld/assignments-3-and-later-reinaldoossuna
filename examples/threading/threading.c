ac760da7d5acfe691d99bd574845dd8694f84c66
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2024-04-09 15:20:04 -0400

    initiliaze data and launch tread

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,24 +10,42 @@
 #define ERROR_LOG(msg, ...) printf("threading ERROR: " msg "\n", ##__VA_ARGS__)
 
 void *threadfunc(void *thread_param) {
+  struct thread_data *data = (struct thread_data *)thread_param;
 
-  // TODO: wait, obtain mutex, wait, release mutex as described by thread_data
-  // structure hint: use a cast like the one below to obtain thread arguments
-  // from your parameter
-  // struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+  // waiting to obtain
+  DEBUG_LOG("Waiting to obtain lock")
+  usleep(data->wait_to_obtain_ms);
+
+  DEBUG_LOG("Locking")
+  int rc = pthread_mutex_lock(data->mutex);
+  if (rc != 0) {
+    ERROR_LOG("failed to obtain lock");
+  } else {
+    data->thread_complete_success = true;
+
+    // waiting to release
+    DEBUG_LOG("Waiting to release lock")
+    usleep(data->wait_to_release_ms);
+    rc = pthread_mutex_unlock(data->mutex);
+    if (rc != 0) {
+      ERROR_LOG("failed to release lock");
+    }
+  }
+
   return thread_param;
 }
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,
                                   int wait_to_obtain_ms,
                                   int wait_to_release_ms) {
-  /**
-   * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass
-   * thread_data to created thread using threadfunc() as entry point.
-   *
-   * return true if successful.
-   *
-   * See implementation details in threading.h file comment block
-   */
-  return false;
+  /* Initiliaze thread data */
+  struct thread_data* data =
+      (struct thread_data*)malloc(sizeof(struct thread_data));
+  data->mutex = mutex;
+  data->wait_to_obtain_ms = wait_to_obtain_ms;
+  data->wait_to_release_ms = wait_to_release_ms;
+  data->thread_complete_success = false;
+
+  /*lauch thread*/
+  return pthread_create(thread, NULL, threadfunc, (void*)data) == 0;
 }
